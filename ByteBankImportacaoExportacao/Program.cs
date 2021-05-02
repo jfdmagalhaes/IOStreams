73612913ab596b2aadceb8734392fc12204257b2
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2021-05-02 19:17:48 -0300

    Close e using

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,37 @@ namespace ByteBankImportacaoExportacao
     { 
         static void Main(string[] args) 
         {
+            var enderecoDoArquivo = "contas.txt";
+
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open)) 
+            {
+                var buffer = new byte[1024];
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer);
+                }
+            }
+     
             Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer);
+            Console.Write(texto);
+            //foreach (var meubyte in buffer)
+            //{
+            //    Console.Write(meubyte);
+            //    Console.Write(" ");
+            //}
+
         }
     }
 } 
